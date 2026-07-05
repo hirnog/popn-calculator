689d8b3aeaec26d7e68cfed7bc7242a54f11a5c9
hirnog <hirnog.0@gmail.com>
2026-07-05 18:05:54 +0900

    fix

--- src/app.js
+++ src/app.js
@@ -3,23 +3,33 @@ function main() {
     registerEvents();
 }
 function registerEvents() {
-    document
-        .getElementById("calculate")
+    document.getElementById("calculate-normal")
         .addEventListener("click", onCalculate);
-    document
-        .getElementById("bpm").addEventListener("keydown", e => {
-            if (e.key === "Enter") {
-                green.focus();
-            }
-        });
+    document.getElementById("calculate-slim")
+        .addEventListener("click", onCalculate);
+    document.getElementById("bpmTgt")
+    .addEventListener("keydown", e => {
+        if (e.key === "Enter") {
+            document.getElementById("bpmMax").focus();
+        }
+    });
+    document.getElementById("bpmMax")
+    .addEventListener("keydown", e => {
+        if (e.key === "Enter") {
+            document.getElementById("bpmMin").focus();
+        }
+    });
 }
 function onCalculate() {
     const config = {
-        bpm: Number(
-            document.getElementById("bpm").value
-        ),
         green: Number(
-            document.getElementById("green").value
+            document.getElementById("bpmTgt").value
+        ),
+        bpmMax: Number(
+            document.getElementById("bpmMax").value
+        ),
+        bpmMin: Number(
+            document.getElementById("bpmMin").value
         ),
         mode:
             document.getElementById("mode").value
